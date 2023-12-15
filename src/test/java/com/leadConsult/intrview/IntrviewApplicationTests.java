c93fd085e476a4900474eb6c556b95ba5019a7df
Nikolay Hristov <nikolayhristovj@gmail.com>
2023-12-15 14:43:21 +0200

    ADD: adding project base structure
    Added pom.xml file, application.yml and gitignore file

---
+++ src/test/java/com/leadConsult/intrview/IntrviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leadConsult.intrview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntrviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

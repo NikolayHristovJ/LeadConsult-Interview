107f56a372607aa3ff7ca424e46773dc31d102db
Nikolay Hristov <nikolayhristovj@gmail.com>
2023-12-15 17:04:11 +0200

    ADD: entities
    Added all entities needed for this project and connected them to the DB

---
+++ src/main/java/com/leadConsult/interview/entity/Student.java
@@ -0,0 +1,49 @@
+package com.leadConsult.interview.entity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "students")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Student {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "student_id")
+  private Long studentId;
+
+  @Column(name = "student_name")
+  private String studentName;
+
+  private Integer age;
+
+  @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+  @JoinTable(
+    name = "students_courses",
+    joinColumns = @JoinColumn(name = "student_id"),
+    inverseJoinColumns = @JoinColumn(name = "course_id")
+  )
+  private Set<Course> studentsCourses;
+
+  @ManyToOne
+  @JoinColumn(name = "group_id", nullable = false)
+  private Group group;
+}
---
+++ src/main/java/com/leadConsult/interview/entity/Teacher.java
@@ -0,0 +1,47 @@
+package com.leadConsult.interview.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "teachers")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Teacher {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "teacher_id")
+  private Long teacherId;
+
+  @Column(name = "teacher_name")
+  private String teacherName;
+
+  private Integer age;
+
+  @ManyToMany
+  @JoinTable(
+    name = "teachers_courses",
+    joinColumns = @JoinColumn(name = "teacher_id"),
+    inverseJoinColumns = @JoinColumn(name = "course_id")
+  )
+  private Set<Course> teachersCourses;
+
+  @ManyToOne
+  @JoinColumn(name = "group_id",nullable = false)
+  private Group group;
+}
